5d9788fd217011d72fafd4cab8a2dd74450a5378
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-05-06 11:06:47 +0700

    build keep high mepas

--- dist/bpptkg-notify.es.js
+++ dist/bpptkg-notify.es.js
@@ -1,4 +1,4 @@
-function d() {
+function u() {
   const e = document.createElement("style");
   document.head.appendChild(e), e.innerHTML = `
       #rsam-popup {
@@ -67,59 +67,58 @@ function d() {
       }
     `;
 }
-function c() {
+function m() {
   const e = document.createElement("div");
   e.style.display = "none", e.id = "rsam-popup", document.body.appendChild(e);
   const t = document.createElement("div");
   t.classList.add("popup-content");
-  const n = document.createElement("span");
-  n.textContent = "×", n.classList.add("close-btn"), t.appendChild(n);
-  const o = document.createElement("div");
-  o.innerHTML = `<svg style="width:50%;height:auto;color: red" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"></path>
-    </svg>`, t.appendChild(o);
+  const o = document.createElement("span");
+  o.textContent = "×", o.classList.add("close-btn"), t.appendChild(o);
   const i = document.createElement("div");
-  i.innerHTML = 'PERINGATAN!!! <br> <div id="rsam-popup-message"></div> <br>', t.appendChild(i), e.appendChild(t), n.addEventListener("click", () => {
+  i.innerHTML = `<svg style="width:50%;height:auto;color: red" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"></path>
+    </svg>`, t.appendChild(i);
+  const n = document.createElement("div");
+  n.innerHTML = 'PERINGATAN!!! <br> <div id="rsam-popup-message"></div> <br>', t.appendChild(n), e.appendChild(t), o.addEventListener("click", () => {
     localStorage.setItem("popup", "off"), e.style.display = "none";
   });
 }
-function u() {
+function f() {
   const e = document.createElement("div");
   e.id = "rsam-pulse", e.style.display = "none", e.innerHTML = '<div class="ringring"></div><div class="circle"></div>', document.body.appendChild(e);
   let t = !1;
-  e.addEventListener("mousedown", function(i) {
-    var l = i.clientX - parseInt(window.getComputedStyle(this).left), r = i.clientY - parseInt(window.getComputedStyle(this).top);
-    function s(a) {
-      t = !0, e.style.left = a.clientX - l + "px", e.style.top = a.clientY - r + "px";
+  e.addEventListener("mousedown", function(n) {
+    var a = n.clientX - parseInt(window.getComputedStyle(this).left), l = n.clientY - parseInt(window.getComputedStyle(this).top);
+    function r(c) {
+      t = !0, e.style.left = c.clientX - a + "px", e.style.top = c.clientY - l + "px";
     }
-    function p() {
+    function d() {
       setTimeout(() => {
         t = !1;
-      }, 2e3), window.removeEventListener("mousemove", s), window.removeEventListener("mouseup", p);
+      }, 2e3), window.removeEventListener("mousemove", r), window.removeEventListener("mouseup", d);
     }
-    window.addEventListener("mousemove", s), window.addEventListener("mouseup", p);
+    window.addEventListener("mousemove", r), window.addEventListener("mouseup", d);
   });
-  let n = 0, o = null;
+  let o = 0, i = null;
   e.addEventListener("click", function() {
-    n++, !o && (o = setTimeout(() => {
-      if (n === 1) {
+    o++, !i && (i = setTimeout(() => {
+      if (o === 1) {
         if (!t) {
-          const i = document.getElementById("rsam-popup");
-          i && (i.style.display = "flex", localStorage.setItem("popup", "on"));
+          const n = document.getElementById("rsam-popup");
+          n && (n.style.display = "flex", localStorage.setItem("popup", "on"));
         }
       } else
         e.style.left = `${window.innerWidth - 70}px`, e.style.top = "0px";
-      n = 0, o = null;
+      o = 0, i = null;
     }, 200));
   });
 }
-d();
-c();
 u();
-const m = new URL(import.meta.url).searchParams.get("url"), f = new EventSource(`${atob(m)}/notify`);
-f.onmessage = function(e) {
-  const t = JSON.parse(e.data), n = document.getElementById("rsam-popup"), o = document.getElementById("rsam-pulse"), i = document.getElementById("rsam-popup-message");
-  if (t.message && (!n || !o))
-    return alert(t.message);
-  t.message ? (n && localStorage.getItem("popup") !== "off" && (n.style.display = "flex"), o && (o.style.display = "block"), i.innerHTML = t.message) : localStorage.setItem("popup", "on");
+m();
+f();
+let s = 0, p = "";
+const g = new URL(import.meta.url).searchParams.get("url"), h = new EventSource(`${atob(g)}/notify`);
+h.onmessage = function(e) {
+  const { mepas: t, date: o, alertType: i } = JSON.parse(e.data), n = document.getElementById("rsam-popup"), a = document.getElementById("rsam-pulse"), l = document.getElementById("rsam-popup-message");
+  n && a && (i ? (p || (p = o), t > s && (s = t), a.style.display = "block", localStorage.getItem("popup") !== "off" && (n.style.display = "flex"), l.innerHTML = i === 1 ? `Nilai RSAM ${s} <br> Waspadai APG > 1KM <br> <span style="font-size:12px;font-weight:normal">${p}</span>` : `Nilai RSAM ${s} <br>Terjadi Gempa VT Kuat <br> <span style="font-size:12px;font-weight:normal">${p}</span>`) : (s = 0, p = "", localStorage.setItem("popup", "on")));
 };

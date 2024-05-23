def5bbd4e4d6f3add6c05cd1d3c6b560bc91dc48
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-05-24 04:52:05 +0700

    fix data

--- api/src/getRsamDataAlt.ts
+++ api/src/getRsamDataAlt.ts
@@ -45,46 +45,23 @@ export const getRsamDataAlt = async () => {
 
     if (medianLastData <= 500) {
       if (!loadingSendTelegram) {
-        console.log("SENDING NOTIFICATION TO TELEGRAM");
-
         loadingSendTelegram = true;
-
-        // try {
-        //   const message = `Terjadi gempa:\nWaktu: ${dayjs(
-        //     eventInProgress
-        //   ).format("YYYY-MM-DD HH:mm:ss")} WIB\nRSAM: ${highRsam}\nDurasi: ${
-        //     Math.round(Date.now() - eventInProgress) / 1000
-        //   } detik`;
-
-        //   const { data } = await axios.post(`https://api.telegram.org/bot6715715865:AAEchBtNy2GlrX-o3ACJQnbTjvv476jBwjY/sendMessage`, {
-        //     chat_id: "-1002026839953",
-        //     text: message,
-        //     parse_mode: "Markdown",
-        //   })
-
-        //   console.log("sent notification to telegram: ", data);
-        // } catch (error) {
-        //   console.log("faild to send notification to telegram: ", error);
-        // }
-
         try {
           const form = new FormData();
-
-          const photoResponse = await fetch(
-            `http://192.168.0.74:1984/api/frame.jpeg?src=main_JUR`
-          );
-          const photo = await photoResponse.blob();
-
           const caption = `Terjadi gempa:\nWaktu: ${dayjs(
             eventInProgress
           ).format("YYYY-MM-DD HH:mm:ss")} WIB\nRSAM: ${highRsam}\nDurasi: ${
             Math.round(Date.now() - eventInProgress) / 1000
           } detik`;
-
           form.append("chat_id", '-1002026839953');
-          form.append("photo", photo);
           form.append("caption", caption);
           form.append("parse_mode", "Markdown");
+
+          const photoResponse = await fetch(
+            `http://192.168.0.74:1984/api/frame.jpeg?src=main_JUR`
+          );
+          const photo = await photoResponse.blob();
+          form.append("photo", photo);
 
           const response = await fetch(
             `https://api.telegram.org/bot6715715865:AAEchBtNy2GlrX-o3ACJQnbTjvv476jBwjY/sendPhoto`,

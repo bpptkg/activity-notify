c39a2eab867f558bf2ef944e7096035f90a2d0ee
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-05-26 06:34:30 +0700

    add delete old file utils

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -1,11 +1,48 @@
-export const findMedian = (numbers: number[]) => {
-    if (numbers.length === 0) throw new Error("No numbers provided");
-    const sortedNumbers = (JSON.parse(JSON.stringify(numbers))).sort((a: number, b: number) => a - b);
-    const midIndex = Math.floor(sortedNumbers.length / 2);
+import fs from "fs";
+import path from "path";
 
-    if (sortedNumbers.length % 2 === 0) {
-        return (sortedNumbers[midIndex - 1] + sortedNumbers[midIndex]) / 2;
-    } else {
-        return sortedNumbers[midIndex];
+export const findMedian = (numbers: number[]) => {
+  if (numbers.length === 0) throw new Error("No numbers provided");
+  const sortedNumbers = JSON.parse(JSON.stringify(numbers)).sort(
+    (a: number, b: number) => a - b
+  );
+  const midIndex = Math.floor(sortedNumbers.length / 2);
+
+  if (sortedNumbers.length % 2 === 0) {
+    return (sortedNumbers[midIndex - 1] + sortedNumbers[midIndex]) / 2;
+  } else {
+    return sortedNumbers[midIndex];
+  }
+};
+
+// Helper function to check if the file is older than 24 hours
+const isOlderThan24Hours = (filePath: string) => {
+  const stats = fs.statSync(filePath);
+  const now = Date.now();
+  const modificationTime = new Date(stats.mtime).getTime();
+  const hours24 = 24 * 60 * 60 * 1000;
+  return now - modificationTime > hours24;
+};
+
+// Function to delete files older than 24 hours
+export const deleteOldFiles = (dirPath: string) => {
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
     }
+
+    files.forEach((file) => {
+      const filePath = path.join(dirPath, file);
+
+      if (file.startsWith("log-") && isOlderThan24Hours(filePath)) {
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error(`Error deleting file: ${filePath}`, err);
+          } else {
+            console.log(`Deleted file: ${filePath}`);
+          }
+        });
+      }
+    });
+  });
 };

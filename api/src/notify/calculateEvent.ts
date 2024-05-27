a7e48fb31779a3fa655e18b74dcadfe771243993
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-05-27 18:28:02 +0700

    wait send tg notification

--- api/src/notify/calculateEvent.ts
+++ api/src/notify/calculateEvent.ts
@@ -8,10 +8,17 @@ import FormData from "form-data";
 import { plotStream } from "./plotStream";
 import axios from "axios";
 
-let eventInProgress = 0;
+let eventInProgress = false;
+let startTime = 0;
 let highRsam = 0;
 let isRf = false;
-let event: {date: string, median: number, data: number[], ratio: number, high?: number} | null = null;
+let event: {
+  date: string;
+  median: number;
+  data: number[];
+  ratio: number;
+  high?: number;
+} | null = null;
 export const calculateEvent = async ({
   mepasJSON,
   melabJSON,
@@ -19,67 +26,78 @@ export const calculateEvent = async ({
   mepasJSON: [string, number][];
   melabJSON: [string, number][];
 }) => {
+  if (eventInProgress) {
+    return
+  }
+
   const lastData = mepasJSON.map((x) => x[1]).slice(-3);
   const medianLastData = findMedian(lastData);
   const date = mepasJSON[mepasJSON.length - 1][0];
   const mepas = Math.round(mepasJSON[mepasJSON.length - 1][1]);
   const melab = Math.round(melabJSON[melabJSON.length - 1][1]);
 
-  if (eventInProgress) {
+  if (startTime) {
     if (highRsam < medianLastData) {
       highRsam = medianLastData;
     }
 
     if (medianLastData <= 750) {
-      const duration = Math.round((Date.now() - eventInProgress) / 1000)
-      const time =  dayjs(eventInProgress).format(
-        "YYYY-MM-DD HH:mm:ss"
-      )
-      const rsam = Math.round(highRsam)
-      const localIsRf = !!isRf
+      const duration = Math.round((Date.now() - startTime) / 1000);
+      const time = dayjs(startTime).format("YYYY-MM-DD HH:mm:ss");
+      const rsam = Math.round(highRsam);
 
-      highRsam = 0;
-      eventInProgress = 0;
-      isRf = false;
-
-      if ((event!.median > 2500 && duration > 10) || (event!.median <= 2500 && duration > 25)) {
-        await eventsDb.update((events) => {
-          events.unshift(event);
-        });
-  
+      if (
+        (event!.median > 2500 && duration > 10) ||
+        (event!.median <= 2500 && duration > 25)
+      ) {
+        eventInProgress = true
         try {
+          await eventsDb.update((events) => {
+            events.unshift(event);
+          });
+
           const form = new FormData();
           const caption = `Terjadi gempa:\nWaktu: ${time} WIB\nRSAM: ${rsam}\nDurasi: ${duration} detik\nRatio: ${event?.ratio}`;
           form.append("chat_id", "-1002026839953");
           form.append("caption", caption);
           form.append("parse_mode", "Markdown");
           await plotStream(event!.date, form);
-  
-          const { data } = await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendPhoto`, form)
+
+          const { data } = await axios.post(
+            `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendPhoto`,
+            form
+          );
           console.log("sent notification to telegram: ", data);
-  
-          if (localIsRf) {
+
+          if (isRf) {
             setTimeout(() => {
-              sendCctv()
+              sendCctv();
             }, 2000);
           }
         } catch (error) {
           console.log("faild to send photo notification to telegram: ", error);
         }
+        eventInProgress = false
       }
+
+      highRsam = 0;
+      startTime = 0;
+      isRf = false;
     }
   } else {
     if (medianLastData > 1000) {
-      eventInProgress = Date.now();
+      eventInProgress = true;
+      startTime = Date.now();
       highRsam = medianLastData;
-      const ratio = Math.round(mepas / melab * Math.pow(10, 2)) / Math.pow(10, 2);
+      const ratio =
+        Math.round((mepas / melab) * Math.pow(10, 2)) / Math.pow(10, 2);
       isRf = ratio <= 2;
       event = {
         date,
         median: medianLastData,
         data: lastData,
         ratio,
-      }
+      };
     }
   }
 
@@ -97,7 +115,6 @@ export const calculateEvent = async ({
         date,
         median: medianLastData,
         data: lastData,
-        status: eventInProgress ? true : false,
       });
     });
   } catch (error) {

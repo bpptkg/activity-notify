2b228d1dd3b6a8c32a9c8296799d1ccb2de32880
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-05-29 09:23:11 +0700

    fix typo

--- api/src/notify/sendCctv.ts
+++ api/src/notify/sendCctv.ts
@@ -2,7 +2,7 @@ export const sendCctv = async (id: string) => {
   try {
     const formPhoto = new FormData();
     formPhoto.append("chat_id", "-1002026839953");
-    formPhoto.append("capion", id);
+    formPhoto.append("caption", id);
     const photoResponse = await fetch(
       `http://192.168.0.74:1984/api/frame.jpeg?src=main_JUR`
     );

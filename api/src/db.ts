958679c7a78239361abe31671c1bcb548b547672
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-05-29 08:21:46 +0700

    add increment db

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,13 +1,20 @@
-import { Memory, Low } from 'lowdb'
-import { JSONFilePreset } from 'lowdb/node'
-import path from 'path'
+import { Memory, Low } from "lowdb";
+import { JSONFilePreset } from "lowdb/node";
+import path from "path";
 
-type Data = {mepas: number, melab: number, date: string, alertType: number}
+type Data = { mepas: number; melab: number; date: string; alertType: number };
 export const memoryDb = new Low<Data>(new Memory(), {
-    mepas: 0,
-    melab: 0,
-    date: '',
-    alertType: 0
-})
+  mepas: 0,
+  melab: 0,
+  date: "",
+  alertType: 0,
+});
 
-export const eventsDb = await JSONFilePreset<any[]>(path.resolve(process.cwd(), './data/events.json'), [])
+export const eventsDb = await JSONFilePreset<any[]>(
+  path.resolve(process.cwd(), "./data/events.json"),
+  []
+);
+export const incrementDb = await JSONFilePreset<{i: number}>(
+  path.resolve(process.cwd(), "./data/increment.json"),
+  { i: 1 }
+);

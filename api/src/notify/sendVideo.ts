00759a4e5a24159ed8f6f1f95c4795f601dd23b7
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-06-07 21:11:56 +0700

    change path

--- api/src/notify/sendVideo.ts
+++ api/src/notify/sendVideo.ts
@@ -6,7 +6,7 @@ import FormData from 'form-data';
 import axios from 'axios';
 import { logger } from '../logger';
 
-const getPath = (date: string) => `/tmp/${date.replaceAll(':', '_').replaceAll(' ', '-')}.mp4`
+const getPath = (date: string) => `/app/videos/${date.replaceAll(':', '_').replaceAll(' ', '-')}.mp4`
 
 export const sendVideoStream = async (date: string) => {
     if (videoDb.data[date] === 'finish') {
@@ -27,8 +27,6 @@ export const sendVideoStream = async (date: string) => {
 }
 
 export const sendVideo = async (date: string) => {
-    const path = `/tmp/${date.replaceAll(':', '_').replaceAll(' ', '-')}.mp4`
-
     const ffmpegCommand = ffmpeg('rtsp://root:pass@192.168.62.154:554/axis-media/media.amp')
         .inputOptions(['-rtsp_transport', 'tcp'])
         .outputOptions(['-c:v', 'libx264'])

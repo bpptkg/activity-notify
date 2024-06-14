0288323924e8fbf428c78c3bfcf1c055a6a7f7e6
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-06-15 06:36:30 +0700

    create record video service

---
+++ record/build.sh
@@ -0,0 +1,2 @@
+docker buildx build --platform linux/amd64 -t tsulatsitamim/b-sinoas-record --push .
+date
---
+++ record/record_video.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Start the file management script in the background
+./manage_files.sh &
+
+# Record video from the webcam for the specified duration
+ffmpeg -i "rtsp://root:pass@192.168.62.154:554/axis-media/media.amp" -c:v copy -f segment -segment_time 15 -reset_timestamps 1 -strftime 1 "/data/%Y%m%d%H%M%S.mp4"

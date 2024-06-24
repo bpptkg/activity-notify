3ebc6e7af56388700dec8a6faf5b5cf76eac3e7e
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-06-25 05:44:37 +0700

    use axios

--- api/src/notify/sendCctv.ts
+++ api/src/notify/sendCctv.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { logger } from "../logger";
 
 export const sendCctv = async (id: string) => {
@@ -5,12 +6,15 @@ export const sendCctv = async (id: string) => {
     const formPhoto = new FormData();
     formPhoto.append("chat_id", "-1002026839953");
     formPhoto.append("caption", id);
-    const photoResponse = await fetch(
-      `http://192.168.0.74:1984/api/frame.jpeg?src=main_JUR`
-    );
-    const photo = await photoResponse.blob();
+
+    const { data: photo } = await axios.get(`http://192.168.0.74:1984/api/frame.jpeg?src=main_JUR`, { responseType: 'blob' })
+
+    // const photoResponse = await fetch(
+    //   `http://192.168.0.74:1984/api/frame.jpeg?src=main_JUR`
+    // );
+    // const photo = await photoResponse.blob();
     formPhoto.append("photo", photo);
-    const data =  await fetch(
+    const data = await fetch(
       `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendPhoto`,
       {
         method: "POST",

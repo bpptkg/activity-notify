6e8833196b764896ec34ef4845074079fe0d5e03
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2024-06-30 20:52:37 +0700

    improve kubah thermal threshold

--- api/src/event/notifyThermalData.ts
+++ api/src/event/notifyThermalData.ts
@@ -9,6 +9,9 @@ const eventInProgres = {
   kubahBd: false
 }
 
+let avgTrigger = false
+let maxTrigger = false
+
 const ucFirst = (str: string) => {
   if (!str) return str;
   return str[0].toUpperCase() + str.slice(1);
@@ -19,14 +22,17 @@ export const notifyThermalData = async (data: ThermalData) => {
 
   await Promise.all(rivers.map(async (river): Promise<void> => {
     if (river === 'kubahBd') {
-      if (!eventInProgres.kubahBd && (data.kubahBd[1] > 20 || data.kubahBdMax[1] > 100)) {
-        eventInProgres.kubahBd = true
+
+      if (!avgTrigger && !maxTrigger && (data.kubahBd[1] > 20 || data.kubahBdMax[1] > 100)) {
+        avgTrigger = data.kubahBd[1] > 20
+        maxTrigger = data.kubahBdMax[1] > 100
+
         const form = new FormData();
         const text = `Peringatan! Terjadi RF/AP di kubah BD. Suhu di kubah BD AVG: ${data.kubahBd[1]} derajat MAX: ${data.kubahBdMax[1]} derajat.\n${data.kubahBd[0]}`;
         form.append("chat_id", "-1002026839953");
         form.append("text", text);
         form.append("parse_mode", "Markdown");
-  
+
         try {
           const { data } = await axios.post(
             `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
@@ -36,8 +42,10 @@ export const notifyThermalData = async (data: ThermalData) => {
         } catch (error) {
           logger.info("failed to send river event to telegram: ", error?.toString());
         }
-      } else if (eventInProgres.kubahBd && data.kubahBd[1] < 15) {
-        eventInProgres.kubahBd = false
+      } else if (avgTrigger && data.kubahBd[1] < 15) {
+        avgTrigger = false
+      } else if (maxTrigger && data.kubahBdMax[1] < 50) {
+        avgTrigger = false
       }
     } else {
       if (data[river][1] > 20 && !eventInProgres[river]) {
@@ -47,7 +55,7 @@ export const notifyThermalData = async (data: ThermalData) => {
         form.append("chat_id", "-1002026839953");
         form.append("text", text);
         form.append("parse_mode", "Markdown");
-  
+
         try {
           const { data } = await axios.post(
             `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
@@ -55,7 +63,7 @@ export const notifyThermalData = async (data: ThermalData) => {
           );
           logger.info("sent event to telegram: ", data);
         } catch (error) {
-          logger.info("failed to send river event to telegram: ",  error?.toString());
+          logger.info("failed to send river event to telegram: ", error?.toString());
         }
       } else if (eventInProgres[river] && data[river][1] < 15) {
         eventInProgres[river] = false
